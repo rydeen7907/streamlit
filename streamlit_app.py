41187f8eca914d63588e374e3da17b5ef59337cb
rydeen7907 <dalakkuma485@gmail.com>
2024-12-07 10:35:13 +0900

    241207-2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,4 @@ import streamlit as st
 st.title("ずんだもんのショーフロク")
 st.write("とりまテスト段階😊")
 st.subheader("プロフィール")
-st.text("プロンプトエンジニア\n,"
-        "Google AI エッセンシャルズ\n,"
-        "Google データアナリティクスプロフェッショナル 取得\n",
-        "無線従事者(アマチュア無線3級)技士 取得 [ JQI3AA ]")
+st.text("プロンプトエンジニア\n,")
