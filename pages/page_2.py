7c06420a2e655f6727704348dfa6877e7a82658d
rydeen7907 <dalakkuma583@gmail.com>
2024-12-07 14:55:03 +0900

    コード修正

--- pages/page_2.py
+++ pages/page_2.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import av
 
 st.title("ずんだもんのショーフロク")
 st.write("まだまだ開発中🚫")
 
-st.video_file("./image/rirakkuma_ramen_1.mp4")
+# 動画ファイルのパス
+video_file = "./image/rirakkuma_ramen_1.mp4"
+
+# 動画を再生する
+st.video(video_file)
+
+# または、動画をストリーミングする
+# with open(video_file, 'rb') as f:
+#   video_bytes = f.read()
+# st.video(video_bytes)
+
+# または、av ライブラリを使用して動画をストリーミングする
+# with open(video_file, "rb") as f:
+#   container = av.open(f)
+#   for frame in container.decode(video=0):
+#     st.image(frame.to_image())
