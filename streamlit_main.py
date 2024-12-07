0c33dcb8bd6d59c43d17fc151da3a8a7a7c1efae
rydeen7907 <dalakkuma583@gmail.com>
2024-12-07 13:32:29 +0900

    241207-9

--- streamlit_main.py
+++ streamlit_main.py
@@ -4,14 +4,19 @@ from PIL import Image
 st.title("ずんだもんのショーフロク")
 st.write("とりまテスト段階😊")
 
+st.subheader(" ＜ 自己紹介 ＞")
+st.text("AIとPythonを使ったコードをYoutubeで公開しています。")
+st.markdown("[Youtube：ずんだもんのショーフロク](https://www.youtube.com/@dalakkuma610)")
+
+
 image = Image.open("./image/kitakata.jpg")
 st.image(image)
 
 st.subheader("プロフィール")
 st.text("1971年 山形県真室川町出身 高卒プロンプトエンジニア")
 st.text("＜ 取得資格 ＞")
-st.text("Google AIエッセンシャルズ")
-st.text ("Google データアナリティクスプロフェッショナル認定")
-st.text("日本化粧品検定協会 化粧品検定3級")
-st.text("8t限定中型自動車運転免許")
-st.text("無線従事者技士免許(アマチュア無線3級) [ JQI3AA ]")
+st.text("Google AIエッセンシャルズ\n",
+        "Google データアナリティクスプロフェッショナル認定\n",
+        "日本化粧品検定協会 化粧品検定3級\n",
+        "8t限定 中型自動車運転免許\n",
+        "無線従事者技士(アマチュア無線3級) [ JQI3AA ]")
